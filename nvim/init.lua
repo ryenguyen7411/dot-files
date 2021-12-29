c7d49a84d750b506d7ac51a595ffe314d9a2f411
Rye Nguyen <ryenguyen7411@gmail.com>
2021-12-30 04:27:33 +0700

    UPDATE: disable coc.nvim in lite mode.

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,12 +19,12 @@ require('packer').startup(function(use)
     end
   }
 
-  use {
-    'neoclide/coc.nvim', branch='release', event='VimEnter',
-    config = function()
-      require('plugins.coc').setup()
-    end
-  }
+  -- use {
+  --   'neoclide/coc.nvim', branch='release', event='VimEnter',
+  --   config = function()
+  --     require('plugins.coc').setup()
+  --   end
+  -- }
 
   use {
     'sindrets/diffview.nvim', event='VimEnter',
