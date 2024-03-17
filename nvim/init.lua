8a2850b60798a8205c5b62e0386472df8af5e3ac
Tan Nguyen <tan.nguyen2@be.com.vn>
2024-03-17 18:01:10 +0700

    feat: add copilot chat

--- nvim/init.lua
+++ nvim/init.lua
@@ -158,20 +158,20 @@ require('lazy').setup({
     end,
   },
 
-  -- {
-  --   "CopilotC-Nvim/CopilotChat.nvim",
-  --   branch = "canary",
-  --   dependencies = {
-  --     { "github/copilot.vim" }, -- or github/copilot.vim
-  --     { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-  --     { "nvim-telescope/telescope.nvim" }, -- for telescope help actions (optional)
-  --   },
-  --   opts = {
-  --     debug = true, -- Enable debugging
-  --     -- See Configuration section for rest
-  --   },
-  --   -- See Commands section for default commands if you want to lazy load on them
-  -- },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" }, -- or github/copilot.vim
+      -- { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      -- { "nvim-telescope/telescope.nvim" }, -- for telescope help actions (optional)
+    },
+    opts = {
+      -- debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 })
 
 require('settings')    -- lua/settings.lua
