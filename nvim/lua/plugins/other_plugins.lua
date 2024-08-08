07edc1369cffaafc296717e0f46abf393b5ea843
Rye Nguyen <ryenguyen7411@gmail.com>
2024-08-09 01:20:40 +0700

    feat: use markview.nvim

--- nvim/lua/plugins/other_plugins.lua
+++ nvim/lua/plugins/other_plugins.lua
@@ -95,13 +95,26 @@ return {
     },
   },
   {
-    'MeanderingProgrammer/' .. 'markdown.nvim',
-    ft = { 'markdown' },
+    'OXY2DEV/markview.nvim',
+    lazy = false, -- Recommended
+
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
     },
     config = function()
-      require('render-markdown').setup {}
+      require('markview').setup {
+        modes = { 'n', 'i', 'no', 'c' },
+        hybrid_modes = { 'i' },
+
+        -- This is nice to have
+        callbacks = {
+          on_enable = function(_, win)
+            vim.wo[win].conceallevel = 2
+            vim.wo[win].conecalcursor = 'nc'
+          end,
+        },
+      }
     end,
   },
   {
