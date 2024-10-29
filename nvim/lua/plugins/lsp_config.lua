854374253723a4a603203a4e436adea8283f4dd9
Rye Nguyen <ryenguyen7411@gmail.com>
2024-10-29 11:18:06 +0700

    feat: update conform

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -193,14 +193,14 @@ end
 M.setup_formatter = function()
   require('conform').setup {
     formatters_by_ft = {
-      javascript = { { 'prettier_d_slim', 'prettier' } },
-      javascriptreact = { { 'prettier_d_slim', 'prettier' } },
-      typescript = { { 'prettier_d_slim', 'prettier' } },
-      typescriptreact = { { 'prettier_d_slim', 'prettier' } },
-      vue = { { 'prettier_d_slim', 'prettier' } },
-      astro = { { 'prettier_d_slim', 'prettier' } },
-      css = { { 'prettier_d_slim', 'prettier' } },
-      html = { { 'prettier_d_slim', 'prettier' } },
+      javascript = { 'prettier_d_slim', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettier_d_slim', 'prettier', stop_after_first = true },
+      typescript = { 'prettier_d_slim', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettier_d_slim', 'prettier', stop_after_first = true },
+      vue = { 'prettier_d_slim', 'prettier', stop_after_first = true },
+      astro = { 'prettier_d_slim', 'prettier', stop_after_first = true },
+      css = { 'prettier_d_slim', 'prettier', stop_after_first = true },
+      html = { 'prettier_d_slim', 'prettier', stop_after_first = true },
       lua = { 'stylua' },
     },
     format_on_save = {
