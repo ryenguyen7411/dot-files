f3ab026df05499517bd80ae8ad7d37e91da198ac
Rye Nguyen <ryenguyen7411@gmail.com>
2025-07-17 02:48:47 +0700

    feat: prepare for minuet-ai.nvim

--- nvim/lua/plugins/gen_ai.lua
+++ nvim/lua/plugins/gen_ai.lua
@@ -74,6 +74,45 @@ M.setup_supermaven = function()
   }
 end
 
+M.setup_minuet = function()
+  return {
+    'milanglacier/minuet-ai.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('minuet').setup {
+        provider = 'codestral', -- Default provider with good performance
+        request_timeout = 3,
+        context_window = 16000,
+        throttle = 1000,
+        debounce = 400,
+        n_completions = 3,
+        add_single_line_entry = true,
+        notify = 'warn',
+        provider_options = {
+          codestral = {
+            optional = {
+              max_tokens = 256,
+              stop = { '\n\n' },
+            },
+          },
+        },
+        -- Virtual text configuration (optional)
+        virtualtext = {
+          auto_trigger_ft = {}, -- Add filetypes you want auto-completion for
+          keymap = {
+            accept = '<A-A>',
+            accept_line = '<A-a>',
+            accept_n_lines = '<A-z>',
+            prev = '<A-[>',
+            next = '<A-]>',
+            dismiss = '<A-e>',
+          },
+        },
+      }
+    end,
+  }
+end
+
 M.setup_code_companion = function()
   local packages = {
     {
@@ -331,6 +370,7 @@ return {
   M.setup_copilot(),
   -- M.setup_copilot_chat(),
   M.setup_supermaven(),
+  -- M.setup_minuet(),
   unpack(M.setup_code_companion()),
   M.setup_opencode_integration(),
 }
