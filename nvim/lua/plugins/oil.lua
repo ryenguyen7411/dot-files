3bfee900a8121aa0dd1a987bdbe996147cce62e2
Rye Nguyen <ryenguyen7411@gmail.com>
2025-08-11 02:33:25 +0700

    feat: try to use oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,12 +2,25 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    skip_confirm_for_simple_edits = true,
+    delete_to_trash = true,
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      max_width = 0.6,
+      max_height = 0.6,
+      get_win_title = function()
+        return 'Oil'
+      end,
+    },
+  },
+  keys = {
+    { '<leader>o', '<cmd>lua require("oil").open_float()<CR>', desc = 'Oil' },
+  },
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
-  config = function()
-    require('oil').setup()
-  end,
 }
